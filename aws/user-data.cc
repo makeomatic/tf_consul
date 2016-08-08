0fe5548be3cfafbdbc642cc13e6c06dbd915016e
Denis Baryshev <dennybaa@gmail.com>
2016-08-08 04:58:14 +0300

    ecs-agent kill retries

--- aws/user-data.cc
+++ aws/user-data.cc
@@ -3,4 +3,4 @@
 runcmd:
   - "curl -V && ( curl -sSL https://get.docker.com/ | sh ) || :"
   - "wget -V && ( wget -qO- https://get.docker.com/ | sh ) || :"
-  - "while (! docker version 2>/dev/null); do sleep 1; done && docker rm -f ecs-agent || :"
+  - "retries=5; while (! docker rm -f ecs-agent); do retries=$((retries-1)); [ $retries -eq 0 ] && break; sleep 2; done"
